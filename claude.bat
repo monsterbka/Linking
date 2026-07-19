be57c3b5fb93966cd376b5be8dcc49b57690dfa4
toantbk <toantbk@gmail.com>
2026-07-19 17:01:45 +0700

    no message

--- claude.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start "" %SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoExit -Command "Set-Location 'D:\Toantbk\Projects\Hackathon'; claude --dangerously-skip-permissions"
